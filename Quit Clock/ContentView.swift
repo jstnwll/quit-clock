9a54d77dba3517c697e6f389284d151ee6803955
Justin Willemsen <125165923+jstnwll@users.noreply.github.com>
2025-06-17 12:23:10 -0800

    Created XCode Project

---
+++ Quit Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quit Clock
+//
+//  Created by Justin Willemsen on 6/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quit Clock/Quit_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quit_ClockApp.swift
+//  Quit Clock
+//
+//  Created by Justin Willemsen on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Quit_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
